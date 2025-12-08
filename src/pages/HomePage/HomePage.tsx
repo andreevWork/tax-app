60b42fdeecb63c50ce4cd97a94b992207b1c2792
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-08 06:51:47 -0800

    fix: add missing main heading on HomePage to pass tests

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ import styles from './HomePage.module.css';
 export const HomePage = () => {
   return (
     <div className={styles.homePageWrapper}>
+      <h1 data-testid="main-heading">Taxes App</h1>
       <CaseSettings />
       <TaxChart />
     </div>
