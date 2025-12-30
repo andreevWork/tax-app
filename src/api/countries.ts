f9fb58f00e42cb13032a892e7e91cbe3bf17fc83
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2025-12-30 21:41:52 +0100

    feat: validate CountriesData

--- src/api/countries.ts
+++ src/api/countries.ts
@@ -1,11 +1,12 @@
 import taxData from '../data/tax.data.json';
-import type { CountriesData, Country } from '../types/taxes';
+import { validateCountriesData } from '../domain/taxes';
+import type { Country } from '../types/taxes';
 import { delay } from '../utils/delay';
 
 const NETWORK_DELAY = 300;
-const data = taxData as CountriesData;
 
 export async function getCountries(): Promise<Country[]> {
   await delay(NETWORK_DELAY);
-  return data.countries;
+  const validated = validateCountriesData(taxData);
+  return validated.countries as Country[];
 }
