b8e270ede7c87bdcd09cd098a1290a68b979c17b
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2026-02-04 15:47:51 +0100

    test(e2e): add country select tests

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test.describe('App E2E Tests', () => {
+test.describe('App', () => {
   test('should load the main page', async ({ page }) => {
     await page.goto('/');
 
