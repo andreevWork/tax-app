0e4084740e263909d5ada39544ac03c00832a024
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2026-02-07 12:47:10 +0100

    fix: typo

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 export { useAppStore } from './app/useAppStore';
 export { useCountryStore } from './country/useCountryStore';
 export { useThemeStore } from './theme/useThemeStore';
-export { useCalculatorInputStore } from './сalculatorInput/useCalculatorInputStore';
+export { useCalculatorInputStore } from './calculatorInput/useCalculatorInputStore';
 export { useTaxResultStore } from './taxResult/useTaxResultStore';
 export { useUserStore } from './user/useUserStore';
