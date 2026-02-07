b132fc9981de9f8c88bc19da79e1b0ee2f8ee7fe
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2026-02-07 18:26:36 +0100

    feat: add logo

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import { CaseSettings } from '../../components/CaseSettings/CaseSettings';
 import { TaxChart } from '../../components/TaxChart/TaxChart';
 import { useCountryStore } from '../../store';
+import shared from '../../styles/shared.module.css';
 import styles from './HomePage.module.css';
 
 export function HomePage() {
@@ -8,7 +9,9 @@ export function HomePage() {
 
   return (
     <div className={styles.homePageWrapper}>
-      <h1 data-testid="main-heading">Taxes Calculator</h1>
+      <h1 data-testid="main-heading" className={shared.visuallyHidden}>
+        Tax Calculator
+      </h1>
       <h2>Current country: {selectedCountry?.name ?? 'No country selected'}</h2>
       <CaseSettings />
       <TaxChart />
